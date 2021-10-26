c727c1046e0daf685f745b34a6157ae028b1ed73
rimji <88181976+rimji@users.noreply.github.com>
2021-10-26 18:18:03 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,7 +32,7 @@
 
         <footer>
              <small><center>
-             <a href="https://www.dirtkill.com/about/">About</a> &nbsp &nbsp <a href="https://www.dirtkill.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/dirtkill.official/">Instagram</a><br>
+             <a href="https://www.dirtkill.com/about/">About</a> &nbsp &nbsp <a href="https://www.dirtkill.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/dirt.kill/">Instagram</a><br>
              &copy; 2021
              </center></small>
         </footer>
