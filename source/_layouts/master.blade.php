b503cd674195b782f944fbf6e7fe179db55f04a3
rimji <88181976+rimji@users.noreply.github.com>
2021-11-08 14:47:10 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -33,7 +33,7 @@
         <footer>
              <small><center>
              <a href="https://www.huubuu.com/about/">About</a> &nbsp &nbsp <a href="https://www.huubuu.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/dirt.kill/">Instagram</a><br>
-             &copy; 2021 HUUBUU
+             &copy; 2021
              </center></small>
         </footer>
     </section>
