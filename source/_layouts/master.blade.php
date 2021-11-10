6cdbf72228f93083447818f28ce337c6df52b39f
rimji <88181976+rimji@users.noreply.github.com>
2021-11-10 22:11:37 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -33,7 +33,7 @@
         <footer>
              <small><center>
              <a href="https://www.pamori.com/about/">About</a> &nbsp &nbsp <a href="https://www.pamori.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/pamori.official/">Instagram</a><br>
-             &copy; 2021 パモリ ラーニング
+             &copy; 2021 パモリ
              </center></small>
         </footer>
     </section>
