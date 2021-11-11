aa0bb4e561f36e441771b0db45d4107fd14744d5
rimji <88181976+rimji@users.noreply.github.com>
2021-11-11 19:17:18 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <center>
     <small> 
-    <img src="https://user-images.githubusercontent.com/88181976/141238091-e776e32c-ff98-46f2-9880-cb96f1f7eea5.png" alt="Pamori" width="50" height="50"><br>
+    <img src="https://user-images.githubusercontent.com/88181976/141238091-e776e32c-ff98-46f2-9880-cb96f1f7eea5.png" alt="Pamori" width="30" height="30"><br>
     PLAY+LEARN
     </small>
     </center>
