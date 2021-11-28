cb6852725aa146d2c0452581540c987d230d90cd
rimji <88181976+rimji@users.noreply.github.com>
2021-11-28 19:15:06 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -33,7 +33,7 @@
         <footer>
              <small><center>
              <a href="https://www.gokks.com/about/">About</a> &nbsp &nbsp <a href="https://www.gokks.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/gokks.official/">Instagram</a><br>
-             &copy; 2021 GOKKS
+             &copy; 2021
              </center></small>
         </footer>
     </section>
