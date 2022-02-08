9e82edd6a53e2797430a1594c40c9a1d3ab83d50
rimji <88181976+rimji@users.noreply.github.com>
2022-02-08 20:12:52 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -6,7 +6,7 @@
    <small><center>
     Body Oil<br><br>
    <p align="justify">
-    Dr. Koll's Body Oil Moisturizer for soft, smooth skin.<br><br>
+    DR. KOLL'S Body Oil Moisturizer for soft, smooth skin.<br><br>
     Handmade using natural and fresh ingredients. Fragrance, colourant and preservative free. Not tested on animals and 100% vegetarian.<br><br>
     Ingredients: Sesamum Indicum Oil<br><br>
     Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees. <a href="https://www.drkolls.com/bocare/"><u>Care Instructions</u></a>
