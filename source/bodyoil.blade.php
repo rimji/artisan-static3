5b0e4e0d6ccc0ed7a052e6403f26960c25148e43
rimji <88181976+rimji@users.noreply.github.com>
2022-02-08 20:05:31 +0530

    Create bodyoil.blade.php

---
+++ source/bodyoil.blade.php
@@ -0,0 +1,15 @@
+@extends('_layouts.master')
+
+@section('title', 'Body Oil')
+
+@section('content')
+   <small><center>
+    Body Oil<br><br>
+   <p align="justify">
+    Rimji<sup>&reg;</sup> Body Oil Moisturizer for soft, smooth skin.<br><br>
+    Handmade using natural and fresh ingredients. Fragrance, colourant and preservative free. Not tested on animals and 100% vegetarian.<br><br>
+    Ingredients: Sesamum Indicum Oil<br><br>
+    Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees. <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a>
+   </p>
+    </center> </small>
+@endsection
