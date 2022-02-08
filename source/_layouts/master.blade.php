c322a9a4d28c8269e9e8230c95fcf075d985a69a
rimji <88181976+rimji@users.noreply.github.com>
2022-02-08 17:04:24 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav> 
-            <a href="https://www.rimji.com"><b>Dr. Koll's</b></a>
+            <a href="https://www.drkolls.com"><b>Dr. Koll's</b></a>
             </nav>
         </header>
 
