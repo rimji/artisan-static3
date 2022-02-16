4c83335f9a3a58d253c26e81836628b3cdc76ca4
rimji <88181976+rimji@users.noreply.github.com>
2022-02-16 19:34:07 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
   <small><center>
-    <a href="https://www.drkolls.com/bodyoil/"><img src="https://user-images.githubusercontent.com/88181976/154278797-911bc10d-1476-40c6-a0d6-93747387efdb.png" alt="Dr. Koll's" width="20" height="20"></a>
+    <a href="https://www.drkolls.com/bodyoil/"><img src="https://user-images.githubusercontent.com/88181976/154278797-911bc10d-1476-40c6-a0d6-93747387efdb.png" alt="Dr. Koll's" width="25" height="25"></a>
  </center> </small>
 @endsection
