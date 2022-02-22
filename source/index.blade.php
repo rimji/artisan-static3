6ad6afe438f7b272cce7144d573aa90c0ea882bc
rimji <88181976+rimji@users.noreply.github.com>
2022-02-22 14:08:11 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
   <small><center>
-    <a href="https://www.drkolls.com/bodyoil/"><img src="https://user-images.githubusercontent.com/88181976/154278797-911bc10d-1476-40c6-a0d6-93747387efdb.png" alt="Dr. Koll's" width="25" height="25"></a>
- </center> </small>
+    
+
+  </center> </small>
 @endsection
