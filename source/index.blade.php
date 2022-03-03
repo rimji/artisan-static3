448647ea8873d845913b2e838605464dec6fd44d
rimji <88181976+rimji@users.noreply.github.com>
2022-03-03 11:17:36 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
   <small><center>
-  <img src="https://user-images.githubusercontent.com/88181976/156503593-8a2c2c60-97c8-421f-8bcf-fc4cd90eb62c.png" alt="Bangle Masters" width="50" height="50">
+  <img src="https://user-images.githubusercontent.com/88181976/156503593-8a2c2c60-97c8-421f-8bcf-fc4cd90eb62c.png" alt="Bangle Masters" width="100" height="100">
+  <br>
   <a href="https://www.instagram.com/banglemasters">@banglemasters</a>
   </center></small>
 @endsection
